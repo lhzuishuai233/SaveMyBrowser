38ac2811ad6f34fbfc4838ff062cdacaaf82530c
Hong Li <Richard.Nonext@outlook.com>
2024-11-26 19:43:18 +0000

    Complete footer.php_1_1126

--- auction/footer.php
+++ auction/footer.php
@@ -1,4 +1,4 @@
-<!-- If you like, you can put a page footer (something that should show up at
+<!-- <!-- If you like, you can put a page footer (something that should show up at
      the bottom of every page, such as helpful links, layout, etc.) here. -->
 
 
@@ -7,6 +7,66 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html> -->
+
+
+
+
+
+<!-- Footer Section -->
+<footer class="footer bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <!-- Footer Links -->
+    <div class="row">
+     // About us 关于我们
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">About Us</h5>
+        <p>
+          We are an online auction platform committed to providing the best auction experience for our users. Feel free to browse our listings or create your own auctions.
+        </p>
+      </div>
+     // Quick links 快速链接
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Quick Links</h5>
+        <ul class="list-unstyled mb-0">
+          <li><a href="index.php" class="text-dark">Home</a></li>
+          <li><a href="mylistings.php" class="text-dark">My Listings</a></li>
+          <li><a href="register.php" class="text-dark">Register</a></li>
+          <li><a href="login_result.php" class="text-dark">Login</a></li>
+        </ul>
+      </div>
+     // Contact us 联系我们
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Contact Us</h5>
+        <ul class="list-unstyled mb-0">
+          <li><i class="fas fa-map-marker-alt"></i> 123 Auction Street, Auction City, AC 12345</li>
+          <li><i class="fas fa-phone"></i> +1 (123) 456-7890</li>
+          <li><i class="fas fa-envelope"></i> support@auctionplatform.com</li>
+        </ul>
+      </div>
+    </div>
+    <!-- Footer Links -->
+  </div>
+
+// Copyright 版权
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2024 Auction Platform: All rights reserved.
+  </div>
+  <!-- Copyright -->
+</footer>
+
+// JavaScript
+<!-- Bootstrap core JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+
+<!-- Font Awesome for Icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
 </body>
 
