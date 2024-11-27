8885e46785b68069e22bef6a326521bf3aa09b9e
Hong Li <Richard.Nonext@outlook.com>
2024-11-27 00:13:45 +0000

    Modify index.php_lh_2

--- auction/footer.php
+++ auction/footer.php
@@ -11,7 +11,7 @@
 </body>
 
 </html> -->
-// test
+<!-- // test -->
 
 
 
@@ -22,14 +22,14 @@
   <div class="container p-4">
     <!-- Footer Links -->
     <div class="row">
-     // About us 关于我们
+     <!-- About us 关于我们 -->
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
         <h5 class="text-uppercase">About Us</h5>
         <p>
           We are an online auction platform committed to providing the best auction experience for our users. Feel free to browse our listings or create your own auctions.
         </p>
       </div>
-     // Quick links 快速链接
+     <!-- Quick links 快速链接 -->
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase">Quick Links</h5>
         <ul class="list-unstyled mb-0">
@@ -39,7 +39,7 @@
           <li><a href="login_result.php" class="text-dark">Login</a></li>
         </ul>
       </div>
-     // Contact us 联系我们
+     <!-- Contact us 联系我们 -->
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase">Contact Us</h5>
         <ul class="list-unstyled mb-0">
@@ -52,15 +52,14 @@
     <!-- Footer Links -->
   </div>
 
-// Copyright 版权
-  <!-- Copyright -->
+  <!-- Copyright 版权 -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2024 Auction Platform: All rights reserved.
   </div>
   <!-- Copyright -->
 </footer>
 
-// JavaScript
+<!-- // JavaScript -->
 <!-- Bootstrap core JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
