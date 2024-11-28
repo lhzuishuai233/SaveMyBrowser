d9c2b94734ee6f4659ef7338fdd344218e4084d7
Shengyu Zhu <ucapszh@ucl.ac.uk>
2024-11-28 13:20:29 +0000

    Modify footer.php to resolve the prob link to error page and display prob

--- auction/footer.php
+++ auction/footer.php
@@ -1,22 +1,5 @@
-<!-- <!-- If you like, you can put a page footer (something that should show up at
+<!-- If you like, you can put a page footer (something that should show up at
      the bottom of every page, such as helpful links, layout, etc.) here. -->
-
-
-
-<!-- Bootstrap core JavaScript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html> -->
-<!-- // test -->
-
-
-
-
-
 <!-- Footer Section -->
 <footer class="footer bg-light text-center text-lg-start">
   <div class="container p-4">
@@ -34,18 +17,15 @@
         <h5 class="text-uppercase">Quick Links</h5>
         <ul class="list-unstyled mb-0">
           <li><a href="index.php" class="text-dark">Home</a></li>
-          <li><a href="mylistings.php" class="text-dark">My Listings</a></li>
           <li><a href="register.php" class="text-dark">Register</a></li>
-          <li><a href="login_result.php" class="text-dark">Login</a></li>
         </ul>
       </div>
      <!-- Contact us 联系我们 -->
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
         <h5 class="text-uppercase">Contact Us</h5>
         <ul class="list-unstyled mb-0">
-          <li><i class="fas fa-map-marker-alt"></i> 123 Auction Street, Auction City, AC 12345</li>
-          <li><i class="fas fa-phone"></i> +1 (123) 456-7890</li>
-          <li><i class="fas fa-envelope"></i> support@auctionplatform.com</li>
+          <li><i class="fas fa-map-marker-alt"></i> University College London, Gower Street, London, WC1E 6BT</li>
+          <li><i class="fas fa-phone"></i> Tel: +44 (0) 20 7679 2000</li>
         </ul>
       </div>
     </div>
@@ -59,14 +39,11 @@
   <!-- Copyright -->
 </footer>
 
-<!-- // JavaScript -->
+
 <!-- Bootstrap core JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
-
-<!-- Font Awesome for Icons -->
-<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
 </body>
 
